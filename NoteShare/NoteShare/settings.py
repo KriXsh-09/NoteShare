77a6ad91a7d691c33b9cf3378fe3a767e5108067
KriXsh-09 <kkakrot09@gmail.com>
2025-10-17 15:22:58 +0530

    Update settings.py

--- NoteShare/NoteShare/settings.py
+++ NoteShare/NoteShare/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
 'django.contrib.sessions',
 'django.contrib.messages',
 'django.contrib.staticfiles',
-'notes.apps.NotesConfig',
+'NoteShare.notes.apps.NotesConfig',
 'cloudinary_storage',
 'cloudinary',
 ]
