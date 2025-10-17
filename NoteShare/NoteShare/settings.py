a006b85d117f89656fed7828c5346541f539f738
KriXsh-09 <kkakrot09@gmail.com>
2025-10-17 12:38:19 +0530

    database_changed

--- NoteShare/NoteShare/settings.py
+++ NoteShare/NoteShare/settings.py
@@ -59,12 +59,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NoteShare.wsgi.application'
 
-
+import dj_database_url
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3',
-}
+'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
