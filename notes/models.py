df09ab832e8cfe4d69dfbec2dedd3874f86fc385
KriXsh-09 <kkakrot09@gmail.com>
2025-10-20 15:57:57 +0530

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
 class Note(models.Model):
-	title = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	file = CloudinaryField('file', resource_type='auto', folder='Notes_uploaded/')
-	uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
-	uploaded_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    file = CloudinaryField('file', resource_type='auto', folder='Notes_uploaded/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return f"{self.title} - {self.uploaded_by.username}"
+    def save(self, *args, **kwargs):
+        # If the uploaded file is PDF, force resource_type='raw'
+        if self.file and str(self.file).endswith('.pdf'):
+            self.file.resource_type = 'raw'
+        super().save(*args, **kwargs)
